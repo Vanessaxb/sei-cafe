2d9354b25461ad381fcaa90b6ad89384d92717ef
Vanessa Beattie <your_git_email@email.com>
2023-09-18 14:07:40 -0500

    Pictures and category rendering

--- models/ItemSchema.js
+++ models/ItemSchema.js
@@ -3,9 +3,11 @@ const item = require('./ItemSchema');
 const Schema = require('mongoose').Schema;
 
 const itemSchema = new Schema({
-  name: { type: String, required: true }, 
-  emoji: String,
+  title: { type: String, required: true }, 
+  image: String,
+  author: String,
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  description: String,
   price: { type: Number, required: true, default: 0 }
 }, {
   timestamps: true
--- src/components/MenuListItem/MenuListItem.js
+++ src/components/MenuListItem/MenuListItem.js
@@ -3,7 +3,8 @@ import styles from './MenuListItem.module.css'
 export default function MenuListItem({ menuItem, handleAddToOrder }) {
     return (
       <div className={styles.MenuListItem}>
-        <div className={styles.emoji + ' ' + 'flex-ctr-ctr'}>{menuItem.emoji}</div>
+       
+        <img src={menuItem.image} width="100px"></img>
         <div className={styles.name}>{menuItem.name}</div>
         <div className={styles.buy}>
           <span>${menuItem.price.toFixed(2)}</span>
